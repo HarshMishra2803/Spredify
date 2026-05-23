65a10b2fee04153924e6344082a05d3ddb2b3598
Harsh Mishra <mishraharsh.2803@gmail.com>
2026-05-23 15:43:22 +0530

    Creating file structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
+import { Routes, Route } from 'react-router-dom'
+import Landing from './pages/Landing'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
+import Create from './pages/Create'
+import History from './pages/History'
+
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-purple-600">
-        Spredify 🚀
-      </h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/create" element={<Create />} />
+      <Route path="/history" element={<History />} />
+    </Routes>
   )
 }
 
